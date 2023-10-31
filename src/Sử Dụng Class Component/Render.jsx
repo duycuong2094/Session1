e3c046b42023e226e58c49d488f0c80728898a7b
duycuong2094 <132628893+duycuong2094@users.noreply.github.com>
2023-10-31 14:24:57 +0700

    Update Render.jsx

--- src/Sử Dụng Class Component/Render.jsx
+++ src/Sử Dụng Class Component/Render.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 
 class Render extends Component {
+    constructor(){
+        super();
+        this.arr=["HTML","Java","Java","Python"]
+    }
     render() {
         return (
             <div className='render'>
                 <h1>Danh Sách Khóa Học</h1>
                 <div>
                     <ul>
-                        <li>HTML</li>
-                        <li>JavaScrip</li>
-                        <li>Python</li>
-                        <li>C#</li>
+                       {this.arr.map((item,index)=>(
+                        <li key={index}>{item}</li>
+                       ))}
                     </ul>
                 </div>
             </div>
